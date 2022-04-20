41a049e3288fd96d210d1dac40a375ffcdb8ea75
KyawKhaing(mehk) <89673231+mehkpentester@users.noreply.github.com>
2022-04-20 15:39:00 +0630

    Create Maximum_of_Two_numbers.py

---
+++ Maximum_of_Two_numbers.py
@@ -0,0 +1,14 @@
+def maximun(a, b):
+    if a >= b:
+        return a
+    
+    else:
+        return b
+
+a = 2
+b = 4
+print(maximun(a,b))
+# -------------------------------------------------
+a =input('a :') #This is input method
+b =input('b :')
+print(maximun(a,b))
