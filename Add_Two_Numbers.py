2a5d8b5e2f37eb0f5df63f08da2eedc85cdb74d6
KyawKhaing(mehk) <89673231+mehkpentester@users.noreply.github.com>
2022-04-20 15:37:26 +0630

    Create Add_Two_Numbers.py

---
+++ Add_Two_Numbers.py
@@ -0,0 +1,8 @@
+num1 = 15
+num2 = 10
+
+sum = num1 + num2 #adding two numbers
+
+print('Sum of {0} nad {1} is {2}' .format(num1, num2, sum))# This is printing value with format method
+
+print(sum) #This is printing only adding result.
